d7873652933c7fb671fa14be84b960638caf6c3a
Your Name <you@example.com>
2021-06-19 10:07:42 +0530

    tenth commit

--- index.js
+++ index.js
@@ -224,6 +224,91 @@ app.post("/register/", async (request, response) => {
       response.send("Student is  Removed")
   })
 
+  app.post("/register/", async (request, response) => {
+    const { username, password, name, gender } = request.body;
+    const isUserRegisteredQuery = `
+              SELECT *
+              FROM user
+              WHERE username = '${username}';`;
+    const isUserRegistered = await database.get(isUserRegisteredQuery);
+    if (isUserRegistered === undefined) {
+      if (password.length < 6) {
+        response.status(400);
+        response.send("Password is too short");
+      } else {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const createUserQuery = `
+              INSERT INTO user
+              (name,username,password,gender)
+              VALUES 
+             ( '${name}',
+             '${username}',
+              '${hashedPassword}',
+              '${gender}')
+              `;
+        await database.run(createUserQuery);
+        response.send("User created successfully");
+      }
+    } else {
+      response.status(400);
+      response.send("User already exists");
+    }
+  });
+  
+  //login the user
+  
+  app.post("/login/", async (request, response) => {
+    const { username, password } = request.body;
+    const isUserFoundQuery = `
+              SELECT * 
+              FROM user
+              WHERE username = '${username}';`;
+    const isUserFound = await database.get(isUserFoundQuery);
+    if (isUserFound === undefined) {
+      response.status(400);
+      response.send("Invalid user");
+    } else {
+      const isPasswordMatched = await bcrypt.compare(
+        password,
+        isUserFound.password
+      );
+      if (isPasswordMatched === true) {
+        const payload = { username: username };
+        const jwtToken = jwt.sign(payload, "SECRET");
+        response.send({ jwtToken });
+        //console.log({ jwtToken });
+      } else {
+        response.status(400);
+        response.send("Invalid password");
+      }
+    }
+  });
+  
+  
+  //API TO DELETE THE STUDENT BASED ON STUDENT_ID 
+  
+  app.delete("/student/:studentId/", async (request,response) => { 
+      const {student_id} = request.params;
+      const deleteStudentQuery = `
+          DELETE FROM 
+              student_table
+          WHERE 
+              student_id = ${studentId};`;
+      await database.run(deleteStudentQuery);
+      response.send("Student is  Removed")
+  })
+  
+  app.delete("/student/:studentId/", async (request,response) => { 
+      const {student_id} = request.params;
+      const deleteStudentQuery = `
+          DELETE FROM 
+              student_table
+          WHERE 
+              student_id = ${studentId};`;
+      await database.run(deleteStudentQuery);
+      response.send("Student is  Removed")
+  })
+
 
 module.exports = app;
 
